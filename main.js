ee76de4507af2e39c3e849fb4d420333c89f08d8
DangKhoa <iunc129@gmail.com>
2020-11-03 18:36:15 +0700

    update

--- main.js
+++
@@ -1,8 +0,0 @@
-window.localStorage;
-localStorage.setItem("countLove", "");
-var count = localStorage.getItem("countLove");
-document.querySelector('#btnLove').onclick = function () {
-    count = Number(count) + Number(1);
-    document.querySelector('#count').innerHTML = count;
-    localStorage.setItem("countLove", count + 1);
-}
