e86f928602bbb197e7386035f2cecc09f2c840fe
DangKhoa <33987075+iunc@users.noreply.github.com>
2026-02-22 13:58:40 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
-
-<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "DÁN_API_KEY_CỦA_BẠN",
+  authDomain: "DÁN_AUTH_DOMAIN",
+  projectId: "DÁN_PROJECT_ID",
+  storageBucket: "DÁN_STORAGE_BUCKET",
+  messagingSenderId: "DÁN_SENDER_ID",
+  appId: "DÁN_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-</script>
