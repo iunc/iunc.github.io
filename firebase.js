0474b5e49aac68b6bfe7cbf6530e6c64d040979e
DangKhoa <33987075+iunc@users.noreply.github.com>
2026-02-22 13:43:07 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
 
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+</script>
