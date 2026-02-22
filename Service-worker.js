b120d7407db34984ae6b1d7a7f392adcde1d821e
DangKhoa <33987075+iunc@users.noreply.github.com>
2026-02-22 23:02:01 +0700

    Create Service-worker.js

---
+++ Service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "ghichu-v1";
+
+const urlsToCache = [
+  "login.html",
+  "dashboard.html",
+  "manifest.json"
+];
+
+// Cài đặt
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Kích hoạt
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch (offline cơ bản)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
